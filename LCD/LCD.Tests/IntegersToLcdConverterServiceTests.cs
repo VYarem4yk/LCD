1a03417a9f5be8ffe7fb090f684a040316a16866
Yaremchuk <Yaremchuk@gmail.com>
2022-02-16 20:57:09 +0300

    Added: test for creation empty grid space.

---
+++ LCD/LCD.Tests/IntegersToLcdConverterServiceTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace LCD.Tests;
+
+public class IntegersToLcdConverterServiceTests
+{
+    [Fact]
+    public void CreateEmptyGridSpace_null_zeroTemplate()
+    {
+        var lcdConverter = new IntegersToLcdConverterService();
+        var emptyGrid = lcdConverter.CreateEmptyGridSpace();
+        const string o = ".";
+        string[,] expectation = { { o, o, o }, { o, o, o }, { o, o, o } };
+        Assert.Equal(expectation, emptyGrid);
+    }
+}
