a2a79a758de602e0802c1ed917cc0e482961a22a
Yaremchuk <Yaremchuk@gmail.com>
2022-02-16 21:47:05 +0300

    Added: mock dictionary representation of digits

---
+++ LCD/LCD.Code/DigitTemplates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCD.Code
+{
+    internal static class DigitTemplates
+    {
+        private static string dot = ".";
+        private static string dash = "_";
+        private static string border = "|";
+        private static Dictionary<int, string[,]> _digitTemplates = new Dictionary<int, string[,]>
+        {
+            [0] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [1] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [2] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [3] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [4] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [5] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [6] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [7] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [8] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+            [9] = new string[,] { { dot, dot, dot }, { dot, dot, dot }, { dot, dot, dot } },
+        };
+    }
+}
