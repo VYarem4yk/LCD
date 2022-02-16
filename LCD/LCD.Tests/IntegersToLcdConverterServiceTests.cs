29249f0b8f4142c9da60dd6825db0cf82da80972
Yaremchuk <Yaremchuk@gmail.com>
2022-02-17 01:13:51 +0300

    Refactored: removed creation of Service instances in each test

--- LCD/LCD.Tests/IntegersToLcdConverterServiceTests.cs
+++ LCD/LCD.Tests/IntegersToLcdConverterServiceTests.cs
@@ -6,11 +6,14 @@ namespace LCD.Tests;
 
 public class IntegersToLcdConverterServiceTests
 {
-
+    IntegersToLcdConverterService lcdConverter;
+    public IntegersToLcdConverterServiceTests ()
+    {
+        lcdConverter = new IntegersToLcdConverterService();
+    }
     [Fact]
     public void CreateEmptyGridSpace_emptyTemplate()
-    {
-        var lcdConverter = new IntegersToLcdConverterService();
+    {        
         var emptyGrid = lcdConverter.GetSpaceTemplate();
         string[,] expectation = { { " " }, {  " " }, {  " " } };
         Assert.Equal(expectation, emptyGrid);
@@ -18,7 +21,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_0_zeroTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(0);
         string[,] expectation = { { ".", "_", "." }, { "|", ".", "|" }, { "|", "_", "|" } };
         Assert.Equal(expectation, actual);
@@ -26,7 +28,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_1_oneTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(1);
         string[,] expectation = { { ".", ".", "." }, { ".", ".", "|" }, { ".", ".", "|" } };
         Assert.Equal(expectation, actual);
@@ -34,7 +35,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_2_twoTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(2);
         string[,] expectation = { { ".", "_", "." }, { ".", "_", "|" }, { "|", "_", "." } };
         Assert.Equal(expectation, actual);
@@ -42,7 +42,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_3_threeTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(3);
         string[,] expectation = { { ".", "_", "." }, { ".", "_", "|" }, { ".", "_", "|" } };
         Assert.Equal(expectation, actual);
@@ -50,7 +49,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_4_fourTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(4);
         string[,] expectation = { { ".", ".", "." }, { "|", "_", "|" }, { ".", ".", "|" } };
         Assert.Equal(expectation, actual);
@@ -58,7 +56,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateGridTemplate_5_fiveTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(5);
         string[,] expectation = { { ".", "_", "." }, { "|", "_", "." }, { ".", "_", "|" } };
         Assert.Equal(expectation, actual);
@@ -66,7 +63,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateZeroGridSpace_6_sixTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(6);
         string[,] expectation = { { ".", "_", "." }, { "|", "_", "." }, { "|", "_", "|" } };
         Assert.Equal(expectation, actual);
@@ -74,7 +70,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateZeroGridSpace_7_sevenTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(7);
         string[,] expectation = { { ".", "_", "." }, { ".", ".", "|" }, { ".", ".", "|" } };
         Assert.Equal(expectation, actual);
@@ -82,7 +77,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateZeroGridSpace_8_eightTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(8);
         string[,] expectation = { { ".", "_", "." }, { "|", "_", "|" }, { "|", "_", "|" } };
         Assert.Equal(expectation, actual);
@@ -90,7 +84,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void CreateZeroGridSpace_9_nineTemplate()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = lcdConverter.GetLcdDigitTemplate(9);
         string[,] expectation = { { ".", "_", "." }, { "|", "_", "|" }, { ".", ".", "|" } };
         Assert.Equal(expectation, actual);
@@ -99,7 +92,6 @@ public class IntegersToLcdConverterServiceTests
     [Fact]
     public void ThrowException_99_NullRefferenceException()
     {
-        var lcdConverter = new IntegersToLcdConverterService();
         var actual = () => lcdConverter.GetLcdDigitTemplate(99);       
         var exception = Assert.Throws<ArgumentException>(actual);
         Assert.Equal("Parameter for GetLcdDigitTemplate should be digit 0-9", exception.Message);
